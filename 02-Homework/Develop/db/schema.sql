b9ece5ac05eb23c767714a010a9e11643e307d08
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-05-02 23:24:51 -0400

    fixed schema

--- 02-Homework/Develop/db/schema.sql
+++ 02-Homework/Develop/db/schema.sql
@@ -4,35 +4,45 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
 CREATE TABLE category (
     id INTEGER NOT NULL AUTO_INCREMENT,
     category_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 
+);
+
 CREATE TABLE product (
-    product_id INTEGER NOT NULL AUTO_INCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(30) NOT NULL,
-    price DECIMAL(2) NOT NULL VALIDATE,
-    stock INTEGER NOT NULL DEFAULT 10 ISNUMERIC,
-    category_id INTEGER FOREIGN KEY REFERENCES(id)
-    PRIMARY KEY (product_id)
+    price DECIMAL(2) NOT NULL,
+    stock INTEGER NOT NULL DEFAULT 10,
+    category_id INTEGER AUTO_INCREMENT,
+    FOREIGN KEY (category_id) REFERENCES category(id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE tag (
-    tag_id INTEGER NOT NULL AUTO_INCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     tag_name VARCHAR(30),
-    PRIMARY KEY (tag_id)
+    PRIMARY KEY (id)
 );
 
 
 CREATE TABLE producttag (
-    pt_id INTEGER NOT NULL AUTO_INCREMENT,
-    product_id INTEGER FOREIGN KEY REFERENCES(product_id),
-    tag_id INTEGER FOREIGN KEY REFERENCES(tag_id),
-    PRIMARY KEY (pt_id)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    product_id INTEGER NOT NULL AUTO_INCREMENT,
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    tag_id INTEGER NOT NULL AUTO_INCREMENT ,
+    FOREIGN KEY (tag_id) REFERENCES tag(id),
+    PRIMARY KEY (id)
 );
 
 
 
 
-
+SELECT * FROM ecommerce_db.category;
+SELECT * FROM ecommerce_db.product;
+SELECT * FROM ecommerce_db.producttag;
+SELECT * FROM ecommerce_db.tag;
