8c1157abebac87f8523df0ca61362a27e0bcbfa3
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-05-03 20:20:07 -0400

    added models into index.js

--- 02-Homework/Develop/models/index.js
+++ 02-Homework/Develop/models/index.js
@@ -6,11 +6,23 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.haveMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+})
 
 module.exports = {
   Product,
